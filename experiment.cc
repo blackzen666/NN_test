942180918782b648382a4f84940e6c30d7da882e
blackzen <blackzen666@gmail.com>
2015-06-10 01:17:04 -0500

    method fixed.

--- experiment.cc
+++ experiment.cc
@@ -25,11 +25,11 @@ int reverseInt (int i) {
 }
 
 
-void read_mnist_data(mat &data,string path,bool train_images) {
+void read_mnist_labels(mat &data) {
   // ifstream file ("t10k-images_unzip");
   
 
-  ifstream file (path);
+  ifstream file ("dataset/t10k-labels");
   if (file.is_open()) {
     int magic_number=0;
     int number_of_images=0;
@@ -41,15 +41,6 @@ void read_mnist_data(mat &data,string path,bool train_images) {
     file.read((char*)&number_of_images,sizeof(number_of_images));
     number_of_images= reverseInt(number_of_images);
     cout << number_of_images <<endl;
-
-    if(train_images == true) {
-      file.read((char*)&n_rows,sizeof(n_rows));
-      n_rows= reverseInt(n_rows);
-      cout << n_rows <<endl;
-      file.read((char*)&n_cols,sizeof(n_cols));
-      n_cols= reverseInt(n_cols);
-      cout <<n_cols <<endl;
-    }
 
 
     int counter = 0;
@@ -76,18 +67,71 @@ void read_mnist_data(mat &data,string path,bool train_images) {
     cout <<data(2,0)<<endl;
     cout <<data(3,0)<<endl;
     cout <<size(data)<<endl;*/
-    if(train_images == true)
-    	{
-    		data.reshape(n_cols*n_rows, number_of_images);
-    		mat bias = ones<mat>(1,number_of_images);
-	  		data = join_vert(data,bias);
-	  		cout << size(data) <<endl; 
-    		//cout << size(data)<<endl;
-    	}
+
   	 	
     cout <<"read counter: "<<counter <<endl;
 	}
 }
+
+
+void read_mnist_images(mat &data) {
+  // ifstream file ();
+  
+
+  ifstream file ("dataset/t10k-images");
+  if (file.is_open()) {
+    int magic_number=0;
+    int number_of_images=0;
+    int n_rows=0;
+    int n_cols=0;
+    file.read((char*)&magic_number,sizeof(magic_number));
+    magic_number= reverseInt(magic_number);
+    cout << magic_number <<endl;
+    file.read((char*)&number_of_images,sizeof(number_of_images));
+    number_of_images= reverseInt(number_of_images);
+    cout << number_of_images <<endl;
+    file.read((char*)&n_rows,sizeof(n_rows));
+    n_rows= reverseInt(n_rows);
+    cout << n_rows <<endl;
+    file.read((char*)&n_cols,sizeof(n_cols));
+    n_cols= reverseInt(n_cols);
+    cout <<n_cols <<endl;
+
+
+    int counter = 0;
+
+    unsigned char  cadena;
+
+////matriz definition because of element read
+    data = zeros<mat>(number_of_images,1);
+
+    while(!file.eof()) {
+
+      file.read((char*)&cadena,sizeof(char));
+      int pixel= cadena;
+      
+      if(counter < number_of_images - 1){
+      		//std::cout << pixel << endl;
+      		data(counter,0)= pixel;
+      		//cout<< data(counter,0) <<endl;
+      	}
+      counter++;
+    }
+    
+    data.reshape(n_cols*n_rows,number_of_images);
+    mat bias = ones<mat>(1,number_of_images);
+  	data = join_vert(data,bias);
+  	cout << size(data) <<endl;
+
+  	 	
+    cout <<"read counter: "<<counter <<endl;
+	}
+}
+
+
+
+
+
 
 
 
@@ -107,23 +151,10 @@ int main() {
 
 	mat X;
 	mat Y;
-  //read_mnist_data("t10k_unzip",false);
-  read_mnist_data(X,"dataset/t10k-images",true);
-  
-  read_mnist_data(Y,"dataset/t10k-labels",false);
-  //cout <<X <<endl;
-  //cout << Y << endl;
-  
 
-  /*
-     mat A = randu<mat>(5,5) * 100;
-     A.print("A:");
-     rowvec B = max(A);
-     B.print("B:");
-     A.each_row() /= B;
-     A.print("C:");
-     mat C = sigmoid(A);
-     C.print("D:");
-     */
+  
+	read_mnist_labels(Y);
+	read_mnist_images(X);
+  
   return 0;
 }
